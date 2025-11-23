9e93e51aba59d9b39a889ded001960d733f7a355
al-faj <142720630+al-faj@users.noreply.github.com>
2025-11-23 18:26:46 +0530

    Fix root directory configuration in vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: './',  
+  root: '.',
   publicDir: 'public',
   build: {
     outDir: 'dist'
   }
 })
+
 
